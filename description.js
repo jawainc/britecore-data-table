3c5e48bb448b134908a222230a60d40cf83be451
Jawad <jawainc@gmail.com>
2019-04-23 01:21:01 +0500

    Create description.js

---
+++ description.js
@@ -0,0 +1,29 @@
+{
+    "basics": {
+      "name": "Jawad Ahmad",
+      "email": "jawainc@gmail.com",
+      "phone": "+923006389129",
+      "summary": "40 yrs old, handsome asian male",
+      "location": {
+        "country": "Pakistan"
+      }
+    },
+    "volunteer": 
+      {
+        "organization": "Ameen-e-Sehat",
+        "website": "http://ameenesehat.org/",
+        "summary": "Develop hospital management software"
+      },
+    "publications": {
+        "name": "Big Dreams",
+        "publisher": "N/A",
+        "summary": "may be in next few years this will be on every book shelf."      
+    },
+    "interests": {
+        "name": "Sports",
+        "keywords": [
+          "Cricket",
+          "Snooker"
+        ]
+    }
+  }
