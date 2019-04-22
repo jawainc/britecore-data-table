b9caddcc83dd67de999bdbbba9f4cdbd66d57bca
Jawad <jawainc@gmail.com>
2019-04-23 01:14:46 +0500

    Update index.js

--- src/firebase/index.js
+++ src/firebase/index.js
@@ -1,12 +1,12 @@
 import {initializeApp} from 'firebase';
 
 const app = initializeApp({
-    apiKey: "AIzaSyBeeujm6Qe6PKknKxVOOPwT7TWegYHuNyI",
-    authDomain: "britecoredatatable-ea7a8.firebaseapp.com",
-    databaseURL: "https://britecoredatatable-ea7a8.firebaseio.com",
-    projectId: "britecoredatatable-ea7a8",
-    storageBucket: "britecoredatatable-ea7a8.appspot.com",
-    messagingSenderId: "923205569170"
+    apiKey: "",
+    authDomain: "",
+    databaseURL: "",
+    projectId: "",
+    storageBucket: "",
+    messagingSenderId: ""
 });
 
 export const db = app.database().ref();
